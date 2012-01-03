4f3e4b2c22f4427ca91f72577fd813363453ee70
Nazar Kulyk <schamane@myeburg.net>
2012-01-03 14:38:20 +0100

    initial commit

---
+++ post_parse.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * @package TS_Post_Parse
+ * @author Nazar Kulyk
+ * @version 1.3
+ */
+/*
+Plugin Name: Telestrekoza Post Parse
+Plugin URI: https://telestrekoza.com/
+Description: Do some needed context replacement by the site.
+Author: Nazar Kulyk
+Version: 1.4
+Author URI: http://myeburg.net/
+*/
+
+namespace ts\plugins;
+
+function ts_post_parse_do($content) {
+	//$content = preg_replace("/(http[s]?:\/\/(www.)?youtube.com\/embed\/([^\?]*)?\??([^\"]*))/", "/combo/youtube/?url=$3", $content);
+	$content = preg_replace('/(http:\/\/(www.)?youtube.com\/embed\/)/', 'https://www.youtube.com/embed/', $content);
+	$content = preg_replace('/(http:\/\/photos.telestrekoza.com\/var\/([^"]*))/', '/link-gallery/$2', $content);
+	//$content = preg_replace('/(<a\s*(?!.*\brel=)[^>]*)(href="[^"]+")((?!.*\brel=)[^>]*)(?:[^>]*)>/', '$1$2$3 rel="nofollow">', $content);
+	//var_dump($content);
+	//exit;
+	return $content;
+}
+
+function ts_post_parse_replaces($content) {
+    if(is_feed())
+	return $content;
+    $content = preg_replace('/="(https?:\/\/(beta\.)?telestrekoza.com\/)/', '="/', $content);
+    //$content = preg_replace('/(<a\s*(?!.*\brel=)[^>]*)(href="[^"]+")((?!.*\brel=)[^>]*)(?:[^>]*)>/', '$1$2$3 rel="nofollow">', $content);
+    //var_dump($content);
+    return $content;
+}
+
+// Now we set that function up to execute when the admin_footer action is called
+add_action('content_save_pre', array(&$this, 'ts_post_parse_do'));
+add_filter('the_content', array(&$this, 'ts_post_parse_replaces'));
+
+?>
